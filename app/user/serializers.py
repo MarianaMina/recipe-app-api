af8807b77d01f816483d35ff043d540cd07a0e71
mmina001 <mmina001@cs.ucy.ac.cy>
2021-03-26 15:12:36 +0200

    Add user endpoints and authentication

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,6 +16,16 @@ class UserSerializer(Serializers.ModelSerializer):
         """ Create new user with encrypted function and return it """
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self,instance,validated_data):
+        """" Update a user, setting the password correctly and return it """
+        password = validated_data.pop('password',None)
+        user = super().update(instance,validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+        return user
+
     class AuthTokenSerializer(Serializers.serializer):
         """ Serializer for the authentication object """
         email = serializers.CharField()
--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics,authentication,permissions
 from user.serializers import Serializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
@@ -13,3 +13,14 @@ class CreateTokenView(ObtainAuthToken):
     """ Create new authentication token for user """
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """ Manage the authenticated user """
+    serializer_class = UserSerializer
+    authentication_classes=(authentication.TokenAuthentication)
+    permissions_classes=(permissions.IsAuthenticated)
+
+    def getObject(self):
+        """ Retrieve and return authenticated user
+         get auth. user and assign it to the request """
+        return self.request.user

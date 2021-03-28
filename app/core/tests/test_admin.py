4cbe0ff0fc96a980d0fd1414e8a50d8ad71c9130
MarianaMina <31707506+MarianaMina@users.noreply.github.com>
2021-03-28 20:56:20 +0300

    Update test_admin.py

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -15,7 +15,7 @@ class AdminSiteTests(TestCase):
         self.user = get_user_model().objects.create_user(
             email='test@gmail.com',
             password='Password123',
-            name='test user full name'
+            name='test user full name',
         )
 
     def test_user_listed(self):
@@ -24,7 +24,7 @@ class AdminSiteTests(TestCase):
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
-    def test_user_change_page(self):
+    def test_user_page_change(self):
         """test user edit page works"""
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
